e14eaa13c5759cfd46ddfcffe2ada7c1ec36ac9f
TelMTProto <117292475+TelMTProto@users.noreply.github.com>
2022-11-08 06:18:05 -0800

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ PORT = 443
 
 # name -> secret (32 hex chars)
 USERS = {
-    "tg":  "00000000000000000000000000000000"
+    "tg":  "32b920dffb51643028e2f6b878d4eac1"
 }
 
 MODES = {
@@ -23,7 +23,7 @@ MODES = {
 TLS_DOMAIN = "www.HideProxi.io"
 
 # Tag for advertising, obtainable from @MTProxybot
-AD_TAG = "27f113165bf94866416e51acfb7dba5d"
+AD_TAG = "d1cd833e886347155cc292f899891ef7"
 
 #7gAAAAAAAAAAAAAAAAAAAAB3d3cuSGlkZVByb3hpLmlv
 #curl -o MTProo.sh -L https://git.io/fjo3u && bash MTProo.sh --port 443 --secret 00000000000000000000000000000000 --tag 8bd7c59cd625d7991b86a77dfbc5fcff --tls www.HideProxi.io --disable-updater
